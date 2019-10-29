6c84694450dda0eb24f46f688556929b3c4a9e22
wangtao <550115082@qq.com>
2019-10-29 21:57:51 +0800

    hot_fix

--- src/main/java/com/sun/git/Happy.java
+++ src/main/java/com/sun/git/Happy.java
@@ -3,5 +3,6 @@ package com.sun.git;
 public class Happy {
 public static void main(String[] args) {
 	System.out.println("Happy right left 111 333334 222 333334567");
+	System.out.println("hot_fix");
 }
 }
