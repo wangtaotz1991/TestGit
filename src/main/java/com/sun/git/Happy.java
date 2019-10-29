413c8630f112181c66822a3b594995a83f68dfdf
wangtao <550115082@qq.com>
2019-10-29 22:21:22 +0800

    11

--- src/main/java/com/sun/git/Happy.java
+++ src/main/java/com/sun/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 public static void main(String[] args) {
 	System.out.println("Happy right left 111 333334 222 333334567");
 	System.out.println("hot_fix111");
+	System.out.println(11);
 }
 }
